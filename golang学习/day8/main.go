91e22bce478d6ab51c8092cc9275ed468b0adbdd
zhougongjin555 <850635521@qq.com>
2022-03-06 20:49:46 +0800

    go day8

---
+++ golang学习/day8/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	x  int64
+	wg sync.WaitGroup
+	m  sync.Mutex
+)
+
+// add 对全局变量x执行5000次加1操作
+func add() {
+	for i := 0; i < 5000; i++ {
+		m.Lock() // 修改x前加锁
+		x = x + 1
+		m.Unlock() // 改完解锁
+	}
+	wg.Done()
+}
+
+func lock_demo() {
+	wg.Add(2)
+
+	go add()
+	go add()
+
+	wg.Wait()
+	fmt.Println(x)
+}
